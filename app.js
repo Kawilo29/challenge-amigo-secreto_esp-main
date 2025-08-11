8ebec32ff4705e6f561610fedfffd5b38c1b5213
Camilo Rojas <kawilo29@gmail.com>
2025-08-10 21:30:25 -0500

    Prueba de array y función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombresPersonas = []
+let nombreAmigo = ""
+let agregarNombre = ""
+
+
+function agregarAmigo() {
+    //let listaNombresPersonas = document.getElementById('amigo');
+    let agregarNombre = prompt("Por favor agrega un nombre:");
+    listaNombresPersonas.push(agregarNombre);
+    console.log(listaNombresPersonas);
+}
+
+agregarAmigo()
+
