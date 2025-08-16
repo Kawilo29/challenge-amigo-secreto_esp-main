7d360b23e3571860eff91534fda4b8987b83258b
Camilo Rojas <kawilo29@gmail.com>
2025-08-16 00:12:00 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 let amigos = [];
 
 function agregarAmigo() {
-<<<<<<< HEAD
     let agregarNombre = document.getElementById('amigo').value;
     if (agregarNombre == "") {
         alert("Por favor, inserte un nombre.");
@@ -47,31 +46,3 @@ function actualizarListaAmigos() {
 
 //agregarAmigo();
 //actualizarListaAmigos();
-=======
-    //let listaNombresPersonas = document.getElementById('amigo');
-    let agregarNombre = prompt("Por favor agrega un nombre:");
-    if (agregarNombre == "") {
-      alerta("Por favor, inserte un nombre.");
-    } else {
-      listaNombresPersonas.push(agregarNombre);
-    }
-   // listaNombresPersonas.push(agregarNombre);
-    console.log(listaNombresPersonas);
-    limpiarCampo()
-}
-
-function limpiarCampo() {
-  document.getElementById(amigo).value "";
-}
-
-
-function actualizarLista() {
-  for 
-
-
-}
-
-
-agregarAmigo()
-
->>>>>>> 75e7e830aaa56095422700e8c7f75baaa9255c8e
